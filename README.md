ad9141841dcd3848d1ff4353943dc917baec52f1
Greg McCoy <gregmccoy@d034.canada.gfa.org>
2016-01-05 10:12:58 -0500

    First commit :D

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # source_replace
-Recursivly replaces text
+recursively replaces text
+```
+usage: replace.py [-h] input
+
+positional arguments:
+  input       Enter folder to search
+
+```
+```
+CSV format -
+
+old_source, new_source
+```
+
+
+Copy list of sources code to source_code.cv in the same directory as the script. The script will recursively search the folder specified and replace all occurances of the source code with it’s new value.
---
+++ replace.py
@@ -0,0 +1,48 @@
+import os
+import re
+import sys
+import csv
+import argparse
+
+def file_match(fname, pat, old, replace):
+    try:
+        f = open(fname, "rt")
+    except IOError:
+        return
+    data = f.readlines()
+    new_data = []
+    f.close()
+    found = False
+    for i, line in enumerate(data):
+        if pat.search(line):
+            print("%s: %i: %s" % (fname, i+1, line))
+            found = True
+        line = line.replace(old, replace)
+        new_data.append(line)
+    if found:
+        with open(fname, "w") as f:
+            for line in new_data:
+                f.write(line)
+            
+def grep(dir_name, s_pat, replace):
+    pat = re.compile(s_pat)
+    for dirpath, dirnames, filenames in os.walk(dir_name):
+        for fname in filenames:
+            fullname = os.path.join(dirpath, fname)
+            file_match(fullname, pat, s_pat, replace)
+
+def sources(path):
+    replaces = []
+    with open("source_codes.csv") as f:
+        reader = csv.reader(f, delimiter=',')
+        for row in reader:
+            replaces.append((row[0], row[1]))
+    for source in replaces:
+        grep(path, source[0], source[1])
+
+#Check for command line
+parser = argparse.ArgumentParser()
+parser.add_argument(dest='input', help="Enter folder to search")
+args = parser.parse_args()
+sources(args.input)
+
